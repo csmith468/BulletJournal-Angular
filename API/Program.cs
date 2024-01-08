ef749f5ebafe966aa29e78dc699ec53e33b7ea74
csmith468 <chapin2816@gmail.com>
2024-01-08 07:36:54 -0600

    update API

--- API/Program.cs
+++ API/Program.cs
@@ -6,17 +6,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors((options) => {
+    options.AddPolicy("client", (corsBuilder) => {
+        corsBuilder.WithOrigins("https://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
+} else {
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
+app.UseCors("client");
 
 app.UseAuthorization();
 
